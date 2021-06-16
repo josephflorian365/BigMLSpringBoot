5c7ebde55cf0485236b99ad4c6278981ed6ca6e1
josephflorian365 <jflorianf@vallegrande.edu.pe>
2021-06-16 11:33:07 -0500

    Prediccion de la especie de flor iris

---
+++ BigMLIris/src/test/java/com/BigMLIris/BigMLIris/BigMlIrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BigMLIris.BigMLIris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMlIrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
